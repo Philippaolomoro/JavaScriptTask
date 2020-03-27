4f0c008cb8d0afc609642fa97a34b1f2a32d81d0
Olomoro Philippa <olomoro.philippa@gmail.com>
2020-03-27 14:36:15 +0100

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = "Olomoro Philippa";
+const courses = ["HTML", "CSS", "Javascript", "python", "nodejs"];
+for (let i = 1; i <= 200; i +=2){
+    if (courses % 2 !== 0){
+        console.log (i);
+    }
+}
+console.log(name);
+console.log(courses);
